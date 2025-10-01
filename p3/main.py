20b99365724e873422427c0b71c16e55567a356e
2025130009 <kevin122106@kau.kr>
2025-10-01 15:34:20 +0900

    Update main.py

---
+++ p3/main.py
@@ -0,0 +1,18 @@
+pw = "Aerospace"
+tries = 5
+att = 0
+
+while att < tries:
+    a=input()
+    
+    if a == pw:
+        print("LOGIN")
+        exit()
+        
+    else:
+        att += 1
+        if att < tries:
+            print("Check again")
+        else:
+            print("Fail")
+            exit()
